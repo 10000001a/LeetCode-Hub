7d1499b28ac3ec0409c7df4892b1bc0aa30be385
10000001a <47503412+10000001a@users.noreply.github.com>
2022-01-16 18:44:19 +0900

    Time: 275 ms (5.03%), Space: 50.8 MB (10.53%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -0,0 +1,78 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+  const m = board.length;
+  const n = board[0].length;    
+  
+  const isVisited = [];
+  
+  for (let i = 0; i < m; i++) {
+    // isVisited.push(new Array(n).fill(true).fill(false, 1, n - 1));
+    isVisited.push(new Array(n).fill(false));
+  }
+
+  for (let i = 1; i < m - 1; i++) {
+    for (let j = 1; j < n - 1; j++) {
+      if (isVisited[i][j] || board[i][j] === 'X') continue;
+
+      let shouldFlip = true;
+      const containingRegion = [];
+      let currentVisiting;
+      const willVisit = [[i, j]];
+
+      while (willVisit.length !== 0) {
+        currentVisiting = willVisit.shift();
+        
+        
+        
+        containingRegion.push([currentVisiting[0], currentVisiting[1]]);
+
+        if (isBorder(currentVisiting)) {
+          shouldFlip = false;
+          continue;
+        }
+        
+        if (board[currentVisiting[0]][currentVisiting[1] + 1] === 'O' && isVisited[currentVisiting[0]][currentVisiting[1] + 1] === false) {
+          willVisit.push([currentVisiting[0], currentVisiting[1] + 1]);
+          isVisited[currentVisiting[0]][currentVisiting[1] + 1] = true;
+        }
+        
+       
+
+        if (board[currentVisiting[0] + 1][currentVisiting[1]] === 'O' && isVisited[currentVisiting[0] + 1][currentVisiting[1]] === false) {
+          willVisit.push([currentVisiting[0] + 1, currentVisiting[1]]);
+          isVisited[currentVisiting[0] + 1][currentVisiting[1]] = true;
+        }
+        
+       
+
+        if (board[currentVisiting[0]][currentVisiting[1] - 1] === 'O' && isVisited[currentVisiting[0]][currentVisiting[1] - 1] === false) {
+          willVisit.push([currentVisiting[0], currentVisiting[1] - 1]);
+          isVisited[currentVisiting[0]][currentVisiting[1] - 1] = true;
+        }
+        
+       
+
+        if (board[currentVisiting[0] - 1][currentVisiting[1]] === 'O' && isVisited[currentVisiting[0] - 1][currentVisiting[1]] === false) {
+          willVisit.push([currentVisiting[0] - 1, currentVisiting[1]]);
+          isVisited[currentVisiting[0] - 1][currentVisiting[1]] = true;
+        }
+      }
+
+      if (shouldFlip) {
+        const len = containingRegion.length;
+        for (let x = 0; x < len; x++) {
+          board[containingRegion[x][0]][containingRegion[x][1]] = 'X';
+        }
+      }
+    }
+  
+    function isBorder(region) {
+      if (region[0] === 0 || region[0] === m - 1 || region[1] === 0 || region[1] === n - 1) return true;
+
+      return false;
+    }
+  }
+}
