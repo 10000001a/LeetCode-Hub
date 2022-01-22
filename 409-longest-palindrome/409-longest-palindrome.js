6e7915a9314ba78d997c372509ad1196596619e0
10000001a <47503412+10000001a@users.noreply.github.com>
2022-01-23 03:01:10 +0900

    Time: 169 ms (5.14%), Space: 40.9 MB (25.37%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+  const letterCount = {};
+  
+  let result = 0;
+  
+  for (let i = 0; i < s.length; i++) {
+    if (letterCount.hasOwnProperty(s[i])) {
+      ++letterCount[s[i]];
+    } else {
+      letterCount[s[i]] = 1;
+    }
+    
+  }
+  
+  let isContainOdd = false;
+  
+  for (const x in letterCount) {
+    if (letterCount[x] % 2 === 1) {
+      isContainOdd = true;
+    }
+    
+    result += (letterCount[x] - (letterCount[x] % 2));
+  }
+  
+  
+  if (isContainOdd) return result + 1
+  
+    return result;
+};
