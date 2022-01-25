f9ebb78b321218a2590cf34c6ce47c87f1aadb53
10000001a <47503412+10000001a@users.noreply.github.com>
2022-01-25 22:43:57 +0900

    Time: 84 ms (88.66%), Space: 45.9 MB (37.48%) - LeetHub

---
+++ 134-gas-station/134-gas-station.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+  let sum = 0;
+  let result = 0;
+  let total = 0;
+  
+  for (let i = 0; i < gas.length; i++) {
+    const margin = gas[i] - cost[i];
+    
+    total += margin
+    if (sum + margin < 0) {
+      result = i + 1;
+      sum = 0;
+      continue;
+    } else {
+      sum += margin;
+    }
+  }
+  
+  if (total < 0) return -1;
+  
+  return result;
+};
