726cc51708f8abcb34bef96b0670e472e27d7a9d
10000001a <47503412+10000001a@users.noreply.github.com>
2022-01-30 17:50:57 +0900

    Time: 3953 ms (5.02%), Space: 45 MB (39.70%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,49 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  const M = grid.length; N = grid[0].length;
+  const movement = [{dx: 0, dy: -1}, {dx: 0, dy: 1}, {dx: -1, dy: 0}, {dx: 1, dy: 0}];
+  let result = 0;
+  
+  
+  
+  const find = () => {
+    for (let i = 0; i < M; i++) {
+      for (let j = 0; j < N; j++) {
+        if (grid[i][j] === "1") {
+          return {i, j};
+        }
+      }
+    }
+    
+    return false;
+  }
+
+                                      
+  const dfs = (i, j) => {
+    grid[i][j] = -1
+                                      
+    for (const {dx, dy} of movement) {
+      const x = i + dx;
+      const y = j + dy;
+      
+      if (x >= 0 && x < M && y >= 0 && y < N && grid[x][y]==="1") {
+        dfs(x, y);
+      }
+    }
+  }
+  
+  
+  while (1) {
+    const next = find();
+    
+    if  (next === false) return result;
+    
+    dfs(next.i, next.j);
+
+    result++;
+  }
+  
+};
