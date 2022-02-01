bccfceedf81475a19f428d0d11b1f3b298f82f78
10000001a <47503412+10000001a@users.noreply.github.com>
2022-02-02 00:08:22 +0900

    Time: 141 ms (10.60%), Space: 43.5 MB (10.14%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -11,20 +11,20 @@
  * @return {Node}
  */
 var cloneGraph = function(node) {
-  const visited = {};
-
+  const visited = {}
+  
   const dfs = (node) => {
-      if (!node) return node;
+    if (!node) return node;
     
-      if (visited[node.val]!=null) return visited[node.val];
-
-      const root = new Node(node.val);
+    if (visited[node.val]) return visited[node.val];
     
-      visited[node.val] = root;
+    const root = new Node(node.val);
     
-      node.neighbors.forEach(n => root.neighbors.push(dfs(n)))
+    visited[node.val] = root;
     
-      return root;
+    root.neighbors = node.neighbors.map(n => dfs(n));
+    
+    return root;
   }
   
   return dfs(node);
